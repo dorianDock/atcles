8cea3182975c7b23d2bd19f2ecc7b7ae64645706
Dorian <gaillotdorian@gmail.com>
2016-09-25 21:55:28 +0200

    adding examples for closures

--- closures.js
+++ closures.js
@@ -1,1 +1,15 @@
 
+var myArray=[1,2,3,4,5,6];
+
+console.log(myArray);
+
+
+console.log(myArray[0]);
+
+function doSomething(){
+  console.log("I'm doing something");
+}
+
+console.log(doSomething);
+
+console.log(doSomething());
